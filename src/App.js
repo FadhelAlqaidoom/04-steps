fd1574f3b20b2b94467bc01b7459c54d7f82c0c3
fadhel <fadhelalqaidoom@icloud.com>
2023-10-01 08:19:13 +0300

    Remove components from App and build them in Components dir

--- src/App.js
+++ src/App.js
@@ -7,9 +7,7 @@ export default function App() {
     <div className="flex min-h-screen justify-center items-center pb-96 text-lg font-bold text-white">
       <div className="grid grid-cols-1 grid-rows-3 items-center gap-20 p-10 bg-gray-50 container mx-96 rounded-xl">
         <Numbers />
-
         <Message />
-
         <Buttons />
       </div>
     </div>
